95ffb948898bffc0bae9790d87ac60b3249d5b3d
oscark6 <oscar.kimani1@student.moringaschool.com>
2024-05-23 13:13:46 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
-
+const router = createBrowserRouter([
+    {
+       path: "/",
+       element: <Home />
+    },
+    {
+        path: "/about",
+        element: <About />
+      },
+      {
+        path: "/login",
+        element: <Login />
+      }
+])
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Home />);
